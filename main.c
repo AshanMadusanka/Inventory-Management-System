53aa95f441b4fb4f4ce372e63434b363c0011ca0
Ashan <2017t00036@stu.cmb.ac.lk>
2025-05-21 20:03:12 +0530

    add displayMessage function

--- main.c
+++ main.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
+#include <stdint.h>
+
+typedef struct {
+
+    char item_name[50];
+    uint16_t item_code;
+    uint16_t quantity;
+    float price;
+
+}Item_t;
+
+Item_t items[50];
+uint32_t item_count;
+
+void displayMessages();
+void addItem();
+void deleteItem();
+void updateItemQuantity();
+void displayTotalInventoryValue();
+
 
 int main(void) {
-    printf("Hello, World!\n");
-    return 0;
+
+}
+
+void displayMessages() {
+
+    printf("Add new item press  (1)\n");
+    printf("Delete item press   (2)\n");
+    printf("Update item quantity    (3)\n");
+    printf("Display total inventory value press (4)\n");
 }
